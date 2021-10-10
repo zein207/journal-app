03c8e7217be89a4f5165cafc32ccb1af3e95f855
David <zein207@gmail.com>
2021-10-10 17:50:15 -0500

    Initial configuration & SASS & Router configuration

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+    return (
+        <div>
+            <Switch>
+
+                <Route path="/auth/login" component={ LoginScreen } exact />
+                <Route path="/auth/register" component={ RegisterScreen } exact />
+
+                <Redirect to="/auth/login" />
+
+            </Switch>       
+        </div>
+    )
+}

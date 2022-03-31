3230d7e3dbd9d36bca33f3ec1796987aaaae4f94
David <zein207@gmail.com>
2022-03-31 17:42:28 -0600

    Add thunk middleware

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,12 @@
-import { types } from '../types/types'
+import { types } from '../types/types';
+
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch( login(123, 'Dua') );
+        }, 3500);
+    }
+}
 
 export const login = (uid, displayName) => ({
     type: types.login,

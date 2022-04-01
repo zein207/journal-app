144487f85572681fbebe68b4982a09e2a380e948
David <zein207@gmail.com>
2022-04-01 11:15:02 -0600

    Register new user

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import validator from 'validator';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 import { removeError, setError } from '../../actions/ui';
 
 import { useForm } from '../../hooks/useForm';
@@ -24,7 +25,7 @@ export const RegisterScreen = () => {
         e.preventDefault();
 
         if( isFormValid() ) {
-            console.log('Form is correct')
+            dispatch( startRegisterWithEmailPasswordName(email, password, name) )
         }
     }
 

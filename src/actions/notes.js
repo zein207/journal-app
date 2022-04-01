cb51d8f8a2f872a79098e6b0ab612a71082117b5
David <zein207@gmail.com>
2022-04-01 16:32:08 -0600

    Active note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebase-config";
+import { types } from "../types/types";
 
 
 export const startNewNote = () => {
@@ -17,6 +18,14 @@ export const startNewNote = () => {
 
         const doc = await db.collection(`${uid}/journal/notes`).add( newNote );
 
-        console.log(doc)
+        dispatch( activeNote( doc.id, newNote ) );
     }
 }
+
+export const activeNote = (id, note) => ({
+    type: types.notesActive,
+    payload: {
+        id,
+        ...note
+    }
+})
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,3 +1,4 @@
+import { types } from "../types/types";
 
 const initialState = {
     notes: [],
@@ -7,6 +8,14 @@ const initialState = {
 export const notesReducer = (state = initialState, action) => {
 
     switch (action.type) {
+
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            }
     
         default:
             return state;

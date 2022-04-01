14be24b74868c9fa6d8deadf4778689ca7669a74
David <zein207@gmail.com>
2022-04-01 16:02:57 -0600

    Create new note

--- src/types/types.js
+++ src/types/types.js
@@ -9,6 +9,14 @@ export const types = {
     uiRemoveError: '[UI] Remove Error',
 
     uiStartLoading: '[UI] Start loading',
-    uiFinishLoading: '[UI] Finish loading'
+    uiFinishLoading: '[UI] Finish loading',
+
+    notesAddNew: '[Notes] New note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdated: '[Notes] Updated note',
+    notesFileUrl: '[Notes] Updated image url',
+    notesDelete: '[Notes] Delete notes',
+    notesLogoutCleaning: '[Notes] Logout cleaning'
 
 }

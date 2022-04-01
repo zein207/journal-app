0609b3a34ff742ad936b88bdac993bbde37a16fb
David <zein207@gmail.com>
2022-04-01 13:57:34 -0600

    Firebase Logout

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch( startLogout() );
+    }
+    
     return (
         <aside className="journal__sidebar">
 
@@ -11,7 +20,7 @@ export const Sidebar = () => {
                     <i className="far fa-moon"></i>
                     <span> Dua Lipa</span>
                 </h3>
-                <button className="btn mt-1">
+                <button className="btn mt-1" onClick={ handleLogout }>
                     Logout
                 </button>
 

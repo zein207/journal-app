85b347e9e8a28dcf742aa44236ea26b94e32062f
David <zein207@gmail.com>
2022-04-01 15:46:07 -0600

    Add notes reducer

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
 
     const dispatch = useDispatch();
+
+    const { name } = useSelector( state => state.auth);
 
     const handleLogout = () => {
         dispatch( startLogout() );
@@ -18,7 +20,7 @@ export const Sidebar = () => {
 
                 <h3 className="mt-1">
                     <i className="far fa-moon"></i>
-                    <span> Dua Lipa</span>
+                    <span> {name}</span>
                 </h3>
                 <button className="btn mt-1" onClick={ handleLogout }>
                     Logout

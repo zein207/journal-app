5c1a9065a9940f3348f65a10cfba51bcc7e39978
David <zein207@gmail.com>
2022-04-02 20:57:44 -0600

    Animate CSS

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -14,7 +14,7 @@ export const JournalEntry = ({ id, date, title, body, url }) => {
     }
 
     return (
-        <div className="journal__entry" onClick={ handleEntryClick }>
+        <div className="journal__entry animate__animated animate__backInLeft animate__faster" onClick={ handleEntryClick }>
 
             {
                 url &&
